9ea60a7319356fc57bc09db0bf51a812922ba234
yosxpeee <yosxpeee@gmail.com>
2025-12-07 15:51:26 +0900

    微修正

--- start.bat
+++ start.bat
@@ -4,5 +4,9 @@ if not exist venv (
     python -m venv venv
     powershell venv\Scripts\activate.ps1
     pip install -r requirements.txt
+    python -m pip install --upgrade pip
+    python image_viewer.py
+) else (
+    powershell venv\Scripts\activate.ps1
+    python image_viewer.py
 )
-python image_viewer.py
