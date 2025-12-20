d838aa5d93fa9fef5955a3bb21ae7118937e44be
yosxpeee <yosxpeee@gmail.com>
2025-12-20 20:02:48 +0900

    表示順序変更

--- panels/right_panel.py
+++ panels/right_panel.py
@@ -174,20 +174,24 @@ class RightPanel:
                 ft.Divider(height=1, color=ft.Colors.with_opacity(0.5, ft.Colors.OUTLINE)),
                 self.make_copyable_text(fileInfo_test, 14)
             ])
+            # 通常のメタデータ(tEXt / zTXt / iTXt)
+            # (ない場合は「なし」と表示する)
             self.metadata_text.controls.extend([
                 ft.Divider(height=1, color=ft.Colors.with_opacity(0.5, ft.Colors.OUTLINE)),
                 ft.Text("PNG メタデータ", weight=ft.FontWeight.BOLD, size=16, color=theme_colors["meta_secondary_title"]),
             ])
-            # tEXt / zTXt / iTXt 解析
+            found_metadata = False
             with open(image_path, "rb") as f:
                 reader = png.Reader(file=f)
                 for chunk_type, data in reader.chunks():
                     ctype = chunk_type.decode("latin1", errors="ignore")
+                    # tEXt (Stable Diffusion WebUIで作成したデータは詳細表示)
                     if ctype == "tEXt":
                         text, prompt_text, negative_text, other_info = get_tEXt(data)
                         if text != "":
                             self.add_divider_and_text(f"{ctype}: ", weight_bold=True)
                             self.metadata_text.controls.append(ft.Text(text))
+                            found_metadata = True
                         else:
                             self.metadata_text.controls.append(ft.Divider(height=1, color=ft.Colors.with_opacity(0.5, ft.Colors.OUTLINE)))
                             if prompt_text != "":
@@ -196,22 +200,31 @@ class RightPanel:
                                 self.add_negative_section(negative_text)
                             if other_info != "":
                                 self.add_other_section(other_info)
-                    elif ctype == "zTXt":
-                        self.add_divider_and_text(f"{ctype}: ", weight_bold=True)
-                        text = get_zTxt(data)
-                        self.metadata_text.controls.append(ft.Text(text))
+                            found_metadata = True
+                    # iTXt (そのまま表示)
                     elif ctype == "iTXt":
                         self.add_divider_and_text(f"{ctype}: ", weight_bold=True)
                         text = get_iTXt(data)
                         self.metadata_text.controls.append(ft.Text(text))
+                        found_metadata = True
+                    # zTXt (そのまま表示)
+                    elif ctype == "zTXt":
+                        self.add_divider_and_text(f"{ctype}: ", weight_bold=True)
+                        text = get_zTxt(data)
+                        self.metadata_text.controls.append(ft.Text(text))
+                        found_metadata = True
+            if found_metadata == False:
+                self.metadata_text.controls.append(ft.Divider(height=1, color=ft.Colors.with_opacity(0.5, ft.Colors.OUTLINE)))
+                self.metadata_text.controls.append(ft.Text("なし"))
             # Stealth PNG Info
+            # (ない場合は項目自体を非表示とする)
             stealth_result = detect_stealth_from_image(image_path)
             if stealth_result:
                 self.metadata_text.controls.extend([
                     ft.Divider(height=1, color=ft.Colors.with_opacity(0.5, ft.Colors.OUTLINE)),
                     ft.Text("Stealth PNG Info", weight=ft.FontWeight.BOLD, size=16, color=theme_colors["meta_secondary_title"]),
                 ])
-                # ロジックを使いまわす
+                # メタデータ(tEXt)のロジックを使いまわす
                 text, prompt_text, negative_text, other_info = get_tEXt(stealth_result['text'].encode('latin1', errors='ignore'))
                 if text != "":
                     self.add_divider_and_text(f"テキスト: ", weight_bold=True)
